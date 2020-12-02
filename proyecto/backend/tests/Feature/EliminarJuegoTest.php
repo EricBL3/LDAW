aeff9d2bd1ec5bf30fc3555588fae3fc49c63876
EricTorres27 <Ilovethisgame17*>
2020-12-02 16:41:25 -0600

    Se terminaron los test

--- proyecto/backend/tests/Feature/EliminarJuegoTest.php
+++ proyecto/backend/tests/Feature/EliminarJuegoTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Support\Facades\DB;
 
 class EliminarJuegoTest extends TestCase
 {
@@ -15,8 +16,8 @@ class EliminarJuegoTest extends TestCase
      */
     public function testExample()
     {
-        $response = $this->get('/');
-
+        $id = 4;
+        $response = $this->deleteJson('/api/juego/borrarJuego/'.$id);
         $response->assertStatus(200);
     }
 }
